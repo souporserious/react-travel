0779a1fdd2c36bf131b54ffe5edcf0dcb9c0e371
Travis Arnold <ftntravis@gmail.com>
2015-10-23 00:48:45 -0700

    added life cycle callbacks

--- src/Travel.jsx
+++ src/Travel.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import shallowCompare from 'react/lib/shallowCompare'
 import CSSPropertyOperations from 'react/lib/CSSPropertyOperations'
 
+const noopProp = func => func
+
 class Travel extends Component {
   static propTypes = {
     to: PropTypes.any,
@@ -11,28 +13,35 @@ class Travel extends Component {
     className: PropTypes.any,
     style: PropTypes.object,
     children: PropTypes.element,
-    getNode: PropTypes.func
+    onMount: PropTypes.func,
+    onUpdate: PropTypes.func,
+    onUnmount: PropTypes.func
   }
 
   static defaultProps = {
     to: document.body,
     tag: 'div',
-    getNode: () => null
+    onMount: noopProp,
+    onUpdate: noopProp,
+    onUnmount: noopProp
   }
 
   componentDidMount() {
-    const { to } = this.props
+    const { to, onMount } = this.props
     const parent = typeof to === 'string' ? document.querySelector(to) : to
     const portal = ReactDOM.findDOMNode(this)
 
     // render to desired location
     parent.appendChild(portal)
 
+    // store the node to allow work to be done on it
+    this._node = onMount(portal) || portal
+
     // store portal to remove later
     this._portal = portal
 
     // render children to portal
-    this._renderPortal(this.props)
+    this._renderPortal(this.props, true)
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -49,12 +58,15 @@ class Travel extends Component {
     // we have to unmount manually
     ReactDOM.unmountComponentAtNode(this._portal)
 
+    // allow cleanup of any DOM work done by user
+    this.props.onUnmount(this._node)
+
     // clean up and remove the portal
     this._portal.parentNode.removeChild(this._portal)
   }
 
-  _renderPortal(props) {
-    const { id, className, style, getNode } = props
+  _renderPortal(props, onMount = false) {
+    const { id, className, style, onUpdate, getNode } = props
     const child = React.Children.only(props.children)
 
     // handle props passed into node
@@ -76,9 +88,10 @@ class Travel extends Component {
       this, child, this._portal
     )
 
-    // we can't use traditional refs with portals
-    // so we pass our newly created node back up
-    getNode(this._portal)
+    // assign new node if updated
+    if(!onMount) {
+      this._node = onUpdate(this._node) || this._node
+    }
   }
 
   render() {
